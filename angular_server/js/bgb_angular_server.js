e2479a29131c29604aad2af313fbd2743929728a
bgbohling <bgbohling@gmail.com>
2015-04-27 12:11:56 -0400

    Use Solstice as Solr interface

--- angular_server/js/bgb_angular_server.js
+++ angular_server/js/bgb_angular_server.js
@@ -2,20 +2,35 @@
 
     Drupal.behaviors.angular_server = {
         attach: function(context, settings){
-            var angularServerApp = angular.module('angularServerApp', []);
+            var angularServerApp = angular.module('angularServerApp', ['solstice']),
+                solrServer = 'http://localhost:8983/solr',
+                $nodes = [];
+            angularServerApp.config(function(SolsticeProvider) {
+                SolsticeProvider.setEndpoint('http://localhost:8983/solr');
+            });
 
-            angularServerApp.controller('angularServerController', function($scope, $http) {
-                var path = 'http://localhost/drupal-7.21/home_page';
-                var $nodes = [];
-                $http.get(path).success(function(response) {
-                    content = (response['content']);
-                    for (var $key in content) {
-                        $nodes[$key] = {};
-                        $nodes[$key]['title'] = content[$key]['title'];
-                        $nodes[$key]['body'] = content[$key]['body'];
-                    }
-                   $scope.content = $nodes;
-                });
+            angularServerApp.scope = {
+                solrUrl: solrServer,
+                displayField: '=',
+                query: '&',
+                results: '&'
+            }
+
+            angularServerApp.controller('angularServerController', function($scope, Solstice) {
+
+
+                Solstice.search({
+                    q: 'tm_body$value:"nostrud premo"',
+                    //fl: 'tm_title, tm_body$value',   // fields that will be returned
+                    //sort: 'title desc',
+                    rows: 20
+                })
+                    .then(function (data){
+                        // now that we've got data, we could, like, apply some Angular filters to it
+                        console.log(data.data.response.docs);
+                        $scope.results = data.data.response.docs;
+                        //console.log(data.data.response.docs);
+                    });
             });
         }
     }
--- angular_server/plugins/templates/angular_server.tpl.php
+++ angular_server/plugins/templates/angular_server.tpl.php
@@ -1,7 +1,8 @@
 <div ng-app="angularServerApp" ng-controller="angularServerController">
   Woo-hoo, stuff from Solr!<br>
-  <div ng-repeat="node in content track by $index">
-    <div class="title" style="font-weight:bold">{{node.title}}</div>
-    <div class="body">{{node.body}}</div>
+
+  <div ng-repeat="node in results track by $index">
+    <div class="title" style="font-weight:bold"><a href="{{node.ss_url}}">{{node.tm_title[0]}}</a></div>
+    <div class="body">{{node.tm_body$value[0]}}</div>
   </div>
 </div>
