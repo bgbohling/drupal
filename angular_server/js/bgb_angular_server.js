792988856cd372139c511a03caffb9ed320d0106
bgbohling <bgbohling@gmail.com>
2015-05-06 11:23:49 -0400

    Filter node list by user-specified content type

--- angular_server/js/bgb_angular_server.js
+++ angular_server/js/bgb_angular_server.js
@@ -2,6 +2,8 @@
 
     Drupal.behaviors.angular_server = {
         attach: function(context, settings){
+            var contentTypes = settings.bgb_angular_server.contentTypes;
+            var typeArray = Object.keys(contentTypes).map(function(key) { return key });
 
             var angularServerApp = angular.module('angularServerApp', ['solstice']),
                 solrServer = 'http://localhost:8983/solr';
@@ -10,6 +12,7 @@
             });
 
             angularServerApp.controller('angularServerController', function($scope, Solstice) {
+                $scope.types = typeArray;
                 Solstice.search({
                     q: '*',
                     fl: 'tm_title, tm_body$value, ss_url, ss_type, ds_changed',   // fields that will be returned
@@ -17,9 +20,28 @@
                     rows: 82
                 })
                     .then(function (data){
-                        console.log(data.data.response.docs);
+                        //console.log(data.data.response.docs);
                         $scope.results = data.data.response.docs;
                     });
+            });
+
+            angularServerApp.filter('filterByType', function() {
+              return function(nodes, type) {
+                  var filtered = [];
+                  console.log(type);
+                  if(type) {
+                      nodes.forEach(function (node) {
+                          if (node.ss_type === type) {
+
+                              filtered.push(node);
+                          }
+                      });
+                      return filtered;
+                  }
+                  else {
+                      return {};
+                  }
+              }
             });
         }
     }
--- angular_server/plugins/templates/angular_server.tpl.php
+++ angular_server/plugins/templates/angular_server.tpl.php
@@ -1,10 +1,8 @@
 <div ng-app="angularServerApp" ng-controller="angularServerController">
-  Woo-hoo, stuff from Solr!<br>
-<div>refine:  <p><input type="text" ng-model="test"></p></div>
-  <div ng-repeat="node in results | filter: test track by $index">
+<div>
+<p>What type of content do you want to see?  <select  ng-model="type" ng-options="type for type in types"></select></p>
+</div>
+  <div ng-repeat="node in results | filterByType:type track by $index">
     <div class="title" style="font-weight:bold"><a href="{{node.ss_url}}">{{node.tm_title[0]}}</a></div>
-    <!--
-    <div class="body">{{node.tm_body$value[0]}}</div>
-     -->
   </div>
 </div>
