3bd4fde78d8db71d2d42decdba0a5b086b292405
bgbohling <bgbohling@gmail.com>
2015-05-06 17:43:55 -0400

    Add filtering by author and default values for filters in select lists

--- angular_server/js/bgb_angular_server.js
+++ angular_server/js/bgb_angular_server.js
@@ -2,8 +2,10 @@
 
     Drupal.behaviors.angular_server = {
         attach: function(context, settings){
-            var contentTypes = settings.bgb_angular_server.contentTypes;
-            var typeArray = Object.keys(contentTypes).map(function(key) { return key });
+            var contentTypes = settings.bgb_angular_server.contentTypes,
+                typeArray = Object.keys(contentTypes).map(function(key) { return key }),
+                authors = settings.bgb_angular_server.authors;
+
 
             var angularServerApp = angular.module('angularServerApp', ['solstice']),
                 solrServer = 'http://localhost:8983/solr';
@@ -11,13 +13,15 @@
                 SolsticeProvider.setEndpoint('http://localhost:8983/solr');
             });
 
+            // use Solr to load up a list of possible nodes
             angularServerApp.controller('angularServerController', function($scope, Solstice) {
                 $scope.types = typeArray;
+                $scope.authors = authors;
                 Solstice.search({
                     q: '*',
-                    fl: 'tm_title, tm_body$value, ss_url, ss_type, ds_changed',   // fields that will be returned
+                    fl: 'tm_title, tm_body$value, ss_url, ss_type, ds_changed, is_author',   // fields that will be returned
                     //sort: 'ss_type desc',
-                    rows: 82
+                    rows: 1000
                 })
                     .then(function (data){
                         //console.log(data.data.response.docs);
@@ -25,23 +29,38 @@
                     });
             });
 
+            // we've got a big list of nodes, now provide some ways of filtering them
             angularServerApp.filter('filterByType', function() {
               return function(nodes, type) {
                   var filtered = [];
-                  console.log(type);
                   if(type) {
-                      nodes.forEach(function (node) {
+                      nodes.forEach(function(node) {
                           if (node.ss_type === type) {
-
                               filtered.push(node);
                           }
                       });
                       return filtered;
                   }
                   else {
-                      return {};
+                      return nodes;
                   }
               }
+            });
+            angularServerApp.filter('filterByAuthor', function() {
+                return function(nodes, author) {
+                    var filtered = [];
+                    if(author) {
+                        nodes.forEach(function(node) {
+                            if (node.is_author == author.uid) {
+                                filtered.push(node);
+                            }
+                        });
+                        return filtered;
+                    }
+                    else {
+                        return nodes;
+                    }
+                }
             });
         }
     }
--- angular_server/plugins/templates/angular_server.tpl.php
+++ angular_server/plugins/templates/angular_server.tpl.php
@@ -1,8 +1,9 @@
 <div ng-app="angularServerApp" ng-controller="angularServerController">
 <div>
-<p>What type of content do you want to see?  <select  ng-model="type" ng-options="type for type in types"></select></p>
+<p>What type of content do you want to see?  <select  ng-model="type" ng-options="type for type in types"><option value="">All types</option></select></p>
+  <p>Who by?  <select  ng-model="author" ng-options="author as author.username for author in authors"><option value="">All authors</option></select></p>
 </div>
-  <div ng-repeat="node in results | filterByType:type track by $index">
+  <div ng-repeat="node in results | filterByType:type | filterByAuthor:author track by $index">
     <div class="title" style="font-weight:bold"><a href="{{node.ss_url}}">{{node.tm_title[0]}}</a></div>
   </div>
 </div>
